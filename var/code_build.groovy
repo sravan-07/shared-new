ff589ad09c1f97db2bda213350428312641f0bde
sravan-07 <85868069+sravan-07@users.noreply.github.com>
2021-09-08 18:08:39 +0530

    Update code_build.groovy

--- var/code_build.groovy
+++ var/code_build.groovy
@@ -1,1 +1,4 @@
-
+def call()
+{
+  checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: '8aa1e2b0-4664-41ba-920a-de369eb199e2', url: 'https://github.com/koddas/war-web-project.git']]])
+}
