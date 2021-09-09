33b2d90a103e21f53fc991b196d555f15f7b2d58
sravan-07 <85868069+sravan-07@users.noreply.github.com>
2021-09-09 16:28:46 +0530

    Update nexus_upload.groovy

--- vars/nexus_upload.groovy
+++ vars/nexus_upload.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-nexusArtifactUploader artifacts: [[artifactId: 'wwp', classifier: '', file: 'target/wwp-1.0.0.war', type: 'war']], credentialsId: 'b57b0d75-f3da-4eb5-a5e0-f00317a8e03d', groupId: 'koddas.web.wa', nexusUrl: '192.168.87.128:8082', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-releases', version: '1.4'
+  nexusArtifactUploader artifacts: [[artifactId: "${artifactid}", classifier: '', file: "${file_path}", type: "${type}"]], credentialsId: 'b57b0d75-f3da-4eb5-a5e0-f00317a8e03d', groupId: "${group_id}", nexusUrl: '192.168.87.128:8082', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-releases', version: "${version}"
 }
