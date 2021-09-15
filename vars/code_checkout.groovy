0c1d14ea6abe394e31f43c7e09784443e683fc0f
sravan-07 <85868069+sravan-07@users.noreply.github.com>
2021-09-15 14:43:35 +0530

    Delete code_checkout.groovy

--- vars/code_checkout.groovy
+++
@@ -1,4 +0,0 @@
-def call()
-{
- checkout([$class: 'GitSCM', branches: [[name: "${branch}"]], extensions: [], userRemoteConfigs: [[credentialsId: '8aa1e2b0-4664-41ba-920a-de369eb199e2', url: "${repo_host}"]]])
-}
