0e88d1acd744a39f27cf102ea71cd65b96bc79fb
sravan-07 <85868069+sravan-07@users.noreply.github.com>
2021-09-15 14:43:48 +0530

    Delete nexus_upload.groovy

--- vars/nexus_upload.groovy
+++
@@ -1,4 +0,0 @@
-def call()
-{
-  nexusArtifactUploader artifacts: [[artifactId: "${artifact_id}", classifier: '', file: "${file_path}", type: "${type}"]], credentialsId: 'b57b0d75-f3da-4eb5-a5e0-f00317a8e03d', groupId: "${group_id}", nexusUrl: '192.168.87.128:8082', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-releases', version: "${version}"
-}
